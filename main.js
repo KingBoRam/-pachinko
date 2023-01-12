0ab4a9a099b46a51a5457917c8b49ed496182b52
KingRamBo <bory2321@naver.com>
2023-01-12 13:09:14 +0900

    -pachinko

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ var menu = [];
 function addmenu(mn){
     menu.push(mn);
     alert('현재 메뉴 : ' + menu.length + '개');
+    showmenu();
+}
+function showmenu(){
+    for(let i=0; i<menu.length; i++){
+        document.write(menu[i]);
+    }
 }
