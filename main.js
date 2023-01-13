127efd1b20c5f4f1a8ba257103203e19a0139912
KingRamBo <bory2321@naver.com>
2023-01-13 13:06:06 +0900

    master

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 var menu = [];
-function addMenu(mn){
-    menu.push(mn);
-    alert('현재 메뉴 : ' + menu.length + '개');
-    clear();
-    showMenu();
+function addMenu(mn) {
+  menu.push(mn);
+  alert("현재 메뉴 : " + menu.length + "개");
+  clear();
+  showMenu();
 }
-function showMenu(){
-    for(let i=0; i<menu.length; i++){
-        document.getElementById("menu").innerText += menu[i]+"\n" ;     
-    }
+function showMenu() {
+  for (let i = 0; i < menu.length; i++) {
+    document.getElementById("menu").innerText += menu[i] + "\n";
+  }
 }
-function clear(){
-    document.getElementById("menu").innerText = "";
+function clear() {
+  document.getElementById("menu").innerText = "";
 }
-function randomMenu(){
-    let pick = Math.floor(Math.random()*menu.length);
-    document.getElementById("menu").innerText = "";
+function writeMenu() {
+  let abc = randomMenu(menu);
+  document.getElementById("random").innerText = abc;
+}
+function randomMenu(array) {
+  let pick = Math.floor(Math.random() * array.length);
+  return array[pick];
 }
