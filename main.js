a06a8b81ba4fbd088d9306f41bc9b505f62d072e
KingRamBo <bory2321@naver.com>
2023-01-19 16:46:20 +0900

    matser

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
-var menu = ["라면", "짜장면", "김치볶음밥"];
+var menu = ["라면", "짜장면", "탕수육", "김치찌개"];
 function addMenu(mn) {
   menu.push(mn);
   alert("현재 메뉴 : " + menu.length + "개");
-  clear();
-  showMenu();
+  clear("list");
+  showMenu("list");
 }
-function showMenu() {
+function showMenu(ex) {
   for (let i = 0; i < menu.length; i++) {
-    document.getElementById("menu").innerText += menu[i] + "\n";
+    document.getElementById(ex).innerText += menu[i] + "\n";
   }
 }
-function clear() {
-  document.getElementById("menu").innerText = "";
+function clear(ex2) {
+  document.getElementById(ex2).innerText = "";
 }
 function randomMenu(array) {
   let pick = Math.floor(Math.random() * array.length);
   return array[pick];
 }
-function writeMenu() {
+function writeMenu(ex3) {
   let abc = randomMenu(menu);
-  document.getElementById("random").innerText = abc;
+  document.getElementById(ex3).innerText = abc;
 }
+
 function effectMenu() {
-  var el = document.querySelector("#clickme");
+  // var el = document.querySelector("#clickme");
+  // el.onclick = function () {
   var dl = document.querySelector("#random");
-  el.onclick = function () {
-    writeMenu();
-    dl.animate(
-      [
-        { transform: "translate(0, -43px)" },
-        { transform: "translate(0, 43px)" },
-      ],
-      {
-        duration: 50,
-        iterations: 15,
-      }
-    );
-  };
+  dl.animate(
+    [
+      { transform: "translate(0, -100px)" },
+      { transform: "translate(0, 100px)" },
+    ],
+    {
+      duration: 70,
+      iterations: 15,
+    }
+  );
+}
+function ff() {
+  clear("random");
+  showMenu("random");
+  effectMenu();
+  setTimeout(() => writeMenu("random"), 1000);
 }
